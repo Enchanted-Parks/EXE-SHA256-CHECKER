e38aee9e21988ce384141b4e3ed9ae2b4dfac88d
Snipeur060 <61784407+Snipeur060@users.noreply.github.com>
2024-08-02 23:08:20 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 from customtkinter import *
 from tkinter import *
+from tkinter import filedialog
 
-
-
-
-# nouvelle fenêtre
-
+#gestion fenêtre
 root = CTk()
-
-
+root.title("Enpa Digest")
 root.geometry("800x600")
+root.resizable(False, False)
+
+
 
 
 
